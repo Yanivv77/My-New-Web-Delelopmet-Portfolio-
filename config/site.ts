6329a4092be75c2728dea15f6c125a4bc474cb4e
Yaniv <Yanivv77@gmail.com>
2024-05-30 17:12:45 +0300

    Add blog footer

--- config/site.ts
+++ config/site.ts
@@ -2,9 +2,10 @@ export const siteConfig = {
   name: "Yaniv Software Blog ",
   url: "https://example.com",
   description: "Nextjs 14 blog using velite, tailwind and shadcn",
-  author: "Yaniv",
+  author: "Yaniv Bialik",
   links: {
     github: "https://github.com/yanivv77",
+    personalSite: "https://yanivprotfolio.com",
   },
 };
 
--- layouts/BlogLayout.tsx
+++ layouts/BlogLayout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { siteConfig } from "@/config/site";
 import { BlogHeader } from "@/components/BlogHeader";
 import { Providers } from "@/components/providers";
+import { BlogFooter } from "@/components/BlogFooter";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -42,6 +43,7 @@ const BlogLayout = ({
           <div className="relative flex min-h-dvh flex-col bg-background dark:bg-black-100">
             <BlogHeader />
             <main className="flex-1">{children}</main>
+            <BlogFooter />
           </div>
         </Providers>
       </div>
