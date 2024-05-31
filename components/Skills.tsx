bc6d8aefb1260bfe57229cdb21d81fe512c773e2
Yaniv <Yanivv77@gmail.com>
2024-05-31 23:09:52 +0300

    Update Skills component to fill the last row with invisible divs if necessary

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,21 +1,24 @@
-import React from "react";
-import Heading from "./Heading";
-import SectionWrapper from "./SectionWrapper";
-import data, { Skill } from "@/data/skills";
-import SkillComponent from "./SkillComponent";
+import React from 'react';
+import Heading from './Heading';
+import SectionWrapper from './SectionWrapper';
+import data, { Skill } from '@/data/skills';
+import SkillComponent from './SkillComponent';
 
 const Skills: React.FC = () => {
-  const numberOfSpacers = data.length % 10 === 0 ? 0 : 10 - (data.length % 2);
+  // Calculate the number of spacers needed to fill the last row completely if it's not full
+  const itemsPerRow = 10; // Desired number of items per row
+  const numberOfSpacers = data.length % itemsPerRow === 0 ? 0 : itemsPerRow - (data.length % itemsPerRow);
 
   return (
     <SectionWrapper>
-      <Heading >skills</Heading>
+      <Heading>Skills</Heading>
       <div className="flex flex-wrap items-stretch gap-5">
         {data.map((skill: Skill) => (
-          <div  key={skill.id}>
+          <div key={skill.id} className="flex-grow">
             <SkillComponent skill={skill} />
           </div>
         ))}
+        {/* Generate invisible divs to fill the remaining space in the last row if necessary */}
         {Array.from({ length: numberOfSpacers }, (_, index) => (
           <div className="flex-grow invisible" key={`spacer-${index}`}></div>
         ))}
