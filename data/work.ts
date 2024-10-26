8b399f69149a83d5a2f3a955db5cd7b2496e75e0
Yaniv Bialik <yanivv77@gmail.com>
2024-10-26 17:05:31 +0300

    update work history

--- data/work.ts
+++ data/work.ts
@@ -13,12 +13,7 @@ const data = [
       { id: 12, name: "Redux", image: "/skills/redux.webp" },
       { id: 27, name: "Jest", image: "/skills/jest.webp" },
     ],
-    options: [
-      "Built and launched over 5 new features using JavaScript, React, Node.js, and MongoDB, and consistently delivered high-quality code through well-organized pull requests.",
-      "Led the development and testing of the crypto exchange section, a key feature that significantly improved system functionality by delivering clean, efficient, and thoroughly tested code.",
-      "Optimized client-side performance by refactoring key sections into reusable components, reducing load times, and improving overall application responsiveness while enhancing code maintainability.",
-      "Improved the deployment process, reducing build times by 40% using Linux subsystems, resulting in faster releases and increased team productivity, enabling quicker feature releases."
-    ],
+   
   },
   {
     id: 2,
@@ -32,11 +27,7 @@ const data = [
       { id: 15, name: "AWS", image: "/skills/aws.webp" },
       { id: 16, name: "Git", image: "/skills/git.webp" }
     ],
-    options: [
-      "Developed a web application from the ground up using C# for the backend and React for the frontend, creating a platform for photographers that sends images to users in real-time using AWS AI recognition.",
-      "Worked in an Agile environment, utilizing Git for version control and implementing CI/CD development practices.",
-      "Set up automated tests, reducing bugs and ensuring consistent deployment, which improved overall team productivity."
-    ],
+    
   },
   {
     id: 3,
@@ -46,12 +37,10 @@ const data = [
     date: "05/2022 - 11/2022",
     skills: [
       { id: 3, name: "Python", image: "/skills/python.webp" },
+      { id: 31, name: "Pytest", image: "/skills/pytest.webp" },
       { id: 30, name: "Linux", image: "/skills/linux.webp" }
     ],
-    options: [
-      "Developed and maintained a Bluetooth Low Energy driver library to support IoT applications.",
-      "Optimized installation processes on Linux servers with Python scripting, improving deployment efficiency by 20%."
-    ],
+    
   },
 ];
 export default data;
